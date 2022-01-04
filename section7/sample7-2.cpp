d92beeff9a8442f6476cc0d9bc8faf38d4eb8336
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2022-01-04 19:00:29 +0900

    update:section7

--- section7/sample7-2.cpp
+++ section7/sample7-2.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 //構造体を定義
 struct Car{
-    int num;
+    int num;//メンバを定義
     string cat;
 };
 
 int main(){
     Car car = {10, "hogehoge"};//宣言と同時に初期化
-    cout << car.num << "\t" << car.cat << endl;
+    cout << car.num << "\t" << car.cat << endl;//ドット演算子でメンバにアクセス
 }
